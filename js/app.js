6c8b2b3398d2a5b72c35996536708f0e73f29e29
Casey Thomas <cpthomas@gmail.com>
2013-02-05 11:26:23 -0500

    Update URL as user moves map around

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ if (typeof app === 'undefined' || !app) {
 // Merge url params with settings set up user
 // URL params take preference
 app.mergeSettings = function() {
-  var params = $.url().param();
+  var params = $.url().fparam();
 
   _.defaults(params, app.opts);
 
@@ -16,17 +16,33 @@ app.mergeSettings = function() {
 app.initMap = function() {
   var settings = app.mergeSettings();
 
-  console.log(settings);
-
   wax.tilejson(settings.tileURL,
   function(tilejson) {
-    var map = new L.Map(settings.mapContainer)
+    app.map = new L.Map(settings.mapContainer)
       .addLayer(new wax.leaf.connector(tilejson))
       .setView(new L.LatLng(settings.lat, settings.lng), settings.zoom);
+
+      app.setEvents();
   });
 };
 
-// Sets lat, lng, and zoom as user pans around map
-app.updateURL = function() {
+app.setEvents = function() {
+  app.map
+    .on('zoomend', function(e) {
+      app.updateURL();
+    })
+    .on('dragend', function(e) {
+      app.updateURL();
+    });
+};
 
+// Sets lat, lng, and zoom as user pans around maps
+app.updateURL = function() {
+  //var params = decodeURIComponent(location.hash.substring(1)).trim().split('&');
+  var zoom = app.map.getZoom(),
+      lat = app.map.getCenter().lat,
+      lng = app.map.getCenter().lng,
+      params = 'zoom=' + zoom + '&lat=' + lat + '&lng=' + lng;
+
+  location.hash = params;
 };
